5ab22959fdf3726909e31e05b6b99aac8b71aec6
rubak <14cse11@gmail.com>
2016-09-19 20:49:59 +0530

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,125 @@
+<html>
+	<head>
+		<title>Radius by TEMPLATED</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<div class="inner">
+					<div class="content">
+						<h1>Family of</h1>
+						<h2>Vefetch technologies<br /></h2>
+						<a href="#" class="button big alt"><span>Let's Go</span></a>
+					</div>
+					<a href="#" class="button hidden"><span>Let's Go</span></a>
+				</div>
+			</header>
+
+		<!-- Main -->
+			<div id="main">
+				<div class="inner">
+					<div class="columns">
+					
+					<?php
+						$i =1;
+						while ($i < 100) {
+						 	echo'<div class="image fit">';
+							echo'<img src="pic/pic ('.$i.').jpg" alt="" />';
+							echo"</div>";
+							$i++;
+						}
+						?>
+
+						 
+					<!-- 		// <div class="image fit">
+							// 	<img src="images/pic01.jpg" alt="" />
+							// </div>
+							// <div class="image fit">
+							// 	<img src="images/pic02.jpeg" alt="" />
+							// </div>
+							// <div class="image fit">
+							// 	<img src="images/pic03.jpg" alt="" />
+							// </div>
+							// <div class="image fit">
+							// 	<img src="images/pic04.jpg" alt="" />
+							// </div>
+
+						
+							// <div class="image fit">
+							// 	<img src="images/pic05.jpg" alt="" />
+							// </div>
+							// <div class="image fit">
+							// 	<img src="images/pic06.jpg" alt="" />
+							// </div>
+							// <div class="image fit">
+							// 	<img src="images/pic07.jpg" alt="" />
+							// </div>
+							// <div class="image fit">
+							// 	<img src="images/pic08.jpg" alt="" />
+							// </div>
+
+					
+							// <div class="image fit">
+							// 	<img src="images/pic09.jpg" alt="" />
+							// </div>
+							// <div class="image fit">
+							// 	<img src="images/pic10.jpg" alt="" />
+							// </div>
+							// <div class="image fit">
+							// 	<img src="images/pic11.png" alt="" />
+							// </div>
+							// <div class="image fit">
+							// 	<img src="images/pic12.jpg" alt="" />
+							// </div>
+
+						
+							// <div class="image fit">
+							// 	<img src="images/pic13.jpg" alt="" />
+							// </div>
+							// <div class="image fit">
+							// 	<img src="images/pic14.jpg" alt="" />
+							// </div>
+							// <div class="image fit">
+							// 	<img src="images/pic15.jpg" alt="" />
+							// </div>
+							// <div class="image fit">
+							// 	<img src="images/pic16.jpg" alt="" />
+							// </div>   -->
+
+					</div>
+				</div>
+			</div>
+
+		<!-- Footer
+			<footer id="footer">
+				<a href="#" class="info fa fa-info-circle"><span>About</span></a>
+				<div class="inner">
+					<div class="content">
+						<h3>Vefetch technologies</h3>
+						<p></p>
+					</div>  -->
+				<!-- 	<div class="copyright">
+						<h3>Follow me</h3>
+						<ul class="icons">
+							<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+							<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+						</ul>
+						&copy; Untitled. Design: <a href="https://templated.co">TEMPLATED</a>. Images: <a href="https://unsplash.com/">Unsplash</a>.
+					</div> -->
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
